9e72b1f1a9ddd0443b550e7da8dcdd01105ea5f3
Onimaru Shohei <70475483+s-onineko@users.noreply.github.com>
2021-04-09 17:50:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import plotly.express as px
 import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 
-desktop_path = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "\\Desktop\\"
+#desktop_path = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "\\Desktop\\"
 
 #============================================================================
 #                                タイトル領域
@@ -125,7 +125,7 @@ if mode == "欲求フラグ判定結果":
         ax.axis("off")
         dt_now = datetime.datetime.now()
         time = dt_now.strftime('%Y%m%d %H%M')        
-        plt.savefig(str(desktop_path) +str(time) + str('-rader')  + str('.jpeg'))
+        plt.savefig(str(time) + str('-rader')  + str('.jpeg'))
 
     labels = ['1', '2', '3', '4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20',\
         '21','22','23','24','25','26','27','28','29','30','31','32','33','34',\

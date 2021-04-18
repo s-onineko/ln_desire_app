0c41490ed770f462d15f4f1bd81149b133b8f265
Onimaru Shohei <70475483+s-onineko@users.noreply.github.com>
2021-04-19 05:11:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,71 @@ st.image(image_1, use_column_width=True)
 #                           欲求フラグ判定結果
 #============================================================================
 
+def download_button(object_to_download, download_filename, button_text, pickle_it=False):
+    if pickle_it:
+        try:
+            object_to_download = pickle.dumps(object_to_download)
+        except pickle.PicklingError as e:
+            st.write(e)
+            return None
+
+    else:
+        if isinstance(object_to_download, bytes):
+            pass
+
+        elif isinstance(object_to_download, pd.DataFrame):
+            object_to_download = object_to_download.to_csv(index=False)
+
+        # Try JSON encode for everything else
+        else:
+            object_to_download = json.dumps(object_to_download)
+
+    try:
+        # some strings <-> bytes conversions necessary here
+        b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    except AttributeError as e:
+        b64 = base64.b64encode(object_to_download).decode()
+
+    button_uuid = str(uuid.uuid4()).replace('-', '')
+    button_id = re.sub('\d+', '', button_uuid)
+
+    custom_css = f""" 
+        <style>
+            #{button_id} {{
+                background-color: rgb(255, 255, 255);
+                color: rgb(38, 39, 48);
+                padding: 0.25em 0.38em;
+                position: relative;
+                text-decoration: none;
+                border-radius: 4px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: rgb(230, 234, 241);
+                border-image: initial;
+            }} 
+            #{button_id}:hover {{
+                border-color: rgb(246, 51, 102);
+                color: rgb(246, 51, 102);
+            }}
+            #{button_id}:active {{
+                box-shadow: none;
+                background-color: rgb(246, 51, 102);
+                color: white;
+                }}
+        </style> """
+
+    dl_link = custom_css + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
+
+    return dl_link
+
+
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+
 #STEP-2
 
 mode = st.sidebar.radio("グラフ化",["設問入力モード","欲求フラグ判定結果"])
@@ -653,78 +718,7 @@ else:
         #df_clst.T.to_csv(str(time) + str('-figure')  + str('-result.csv'))
         
 
-        def download_button(object_to_download, download_filename, button_text, pickle_it=False):
-            if pickle_it:
-                try:
-                    object_to_download = pickle.dumps(object_to_download)
-                except pickle.PicklingError as e:
-                    st.write(e)
-                    return None
-
-            else:
-                if isinstance(object_to_download, bytes):
-                    pass
-
-                elif isinstance(object_to_download, pd.DataFrame):
-                    object_to_download = object_to_download.to_csv(index=False)
-
-                # Try JSON encode for everything else
-                else:
-                    object_to_download = json.dumps(object_to_download)
-
-            try:
-                # some strings <-> bytes conversions necessary here
-                b64 = base64.b64encode(object_to_download.encode()).decode()
-
-            except AttributeError as e:
-                b64 = base64.b64encode(object_to_download).decode()
-
-            button_uuid = str(uuid.uuid4()).replace('-', '')
-            button_id = re.sub('\d+', '', button_uuid)
-
-            custom_css = f""" 
-                <style>
-                    #{button_id} {{
-                        background-color: rgb(255, 255, 255);
-                        color: rgb(38, 39, 48);
-                        padding: 0.25em 0.38em;
-                        position: relative;
-                        text-decoration: none;
-                        border-radius: 4px;
-                        border-width: 1px;
-                        border-style: solid;
-                        border-color: rgb(230, 234, 241);
-                        border-image: initial;
-                    }} 
-                    #{button_id}:hover {{
-                        border-color: rgb(246, 51, 102);
-                        color: rgb(246, 51, 102);
-                    }}
-                    #{button_id}:active {{
-                        box-shadow: none;
-                        background-color: rgb(246, 51, 102);
-                        color: white;
-                        }}
-                </style> """
-
-            dl_link = custom_css + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
-
-            return dl_link
-
-
-        def file_selector(folder_path='.'):
-            filenames = os.listdir(folder_path)
-            selected_filename = st.selectbox('Select a file', filenames)
-            return os.path.join(folder_path, selected_filename)
-
-
         if __name__ == '__main__':
-            st.markdown('-'*17)
-
-
-            # ---------------------
-            # Download from memory
-            # ---------------------
             if st.checkbox('Download object from memory'):
                 st.write('~> Use if you want to save some data from memory (e.g. pd.DataFrame, dict, list, str, int)')
 

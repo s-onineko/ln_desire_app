64fd6ee66b98bd87e6766b407658b714f4785a29
Onimaru Shohei <70475483+s-onineko@users.noreply.github.com>
2021-04-19 04:00:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ import datetime
 import plotly.express as px
 import plotly.graph_objs as go
 import matplotlib.pyplot as plt
+import base64
+import os
+import json
+import pickle
+import uuid
+import re
 
 #desktop_path = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "\\Desktop\\"
 
@@ -646,15 +652,89 @@ else:
         time = dt_now.strftime('%Y%m%d %H%M')        
         #df_clst.T.to_csv(str(time) + str('-figure')  + str('-result.csv'))
         
-        def get_table_download_link_csv(df):
-            #csv = df.to_csv(index=False)
-            csv = df.to_csv().encode()
-            #b64 = base64.b64encode(csv.encode()).decode() 
-            b64 = base64.b64encode(csv).decode()
-            href = f'<a href="data:file/csv;base64,{b64}" download="captura.csv" target="_blank">Download csv file</a>'
-            return href
-        get_table_download_link_csv(df_clst.T)
-        st.markdown(get_table_download_link_csv(df_clst), unsafe_allow_html=True)
+
+        def download_button(object_to_download, download_filename, button_text, pickle_it=False):
+            """
+            Generates a link to download the given object_to_download.
+
+            Params:
+            ------
+            object_to_download:  The object to be downloaded.
+            download_filename (str): filename and extension of file. e.g. mydata.csv,
+            some_txt_output.txt download_link_text (str): Text to display for download
+            link.
+            button_text (str): Text to display on download button (e.g. 'click here to download file')
+            pickle_it (bool): If True, pickle file.
+
+            Returns:
+            -------
+            (str): the anchor tag to download object_to_download
+
+            Examples:
+            --------
+            download_link(your_df, 'YOUR_DF.csv', 'Click to download data!')
+            download_link(your_str, 'YOUR_STRING.txt', 'Click to download text!')
+
+            """
+            if pickle_it:
+                try:
+                    object_to_download = pickle.dumps(object_to_download)
+                except pickle.PicklingError as e:
+                    st.write(e)
+                    return None
+
+            else:
+                if isinstance(object_to_download, bytes):
+                    pass
+
+                elif isinstance(object_to_download, pd.DataFrame):
+                    object_to_download = object_to_download.to_csv(index=False)
+
+                # Try JSON encode for everything else
+                else:
+                    object_to_download = json.dumps(object_to_download)
+
+            try:
+                # some strings <-> bytes conversions necessary here
+                b64 = base64.b64encode(object_to_download.encode()).decode()
+
+            except AttributeError as e:
+                b64 = base64.b64encode(object_to_download).decode()
+
+            button_uuid = str(uuid.uuid4()).replace('-', '')
+            button_id = re.sub('\d+', '', button_uuid)
+
+            custom_css = f""" 
+                <style>
+                    #{button_id} {{
+                        background-color: rgb(255, 255, 255);
+                        color: rgb(38, 39, 48);
+                        padding: 0.25em 0.38em;
+                        position: relative;
+                        text-decoration: none;
+                        border-radius: 4px;
+                        border-width: 1px;
+                        border-style: solid;
+                        border-color: rgb(230, 234, 241);
+                        border-image: initial;
+
+                    }} 
+                    #{button_id}:hover {{
+                        border-color: rgb(246, 51, 102);
+                        color: rgb(246, 51, 102);
+                    }}
+                    #{button_id}:active {{
+                        box-shadow: none;
+                        background-color: rgb(246, 51, 102);
+                        color: white;
+                        }}
+                </style> """
+
+            dl_link = custom_css + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
+
+            return dl_link
+        
+        
         '''
         入力結果をCSVファイルで出力します。ダウンロードしたファイルを用いて「欲求フラグ判定」にてレーダーチャートを確認できます。
         '''

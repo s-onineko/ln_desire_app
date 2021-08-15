35c3e8c8e76fc40cffbee51293a7f9c18e622e52
Onimaru Shohei <70475483+s-onineko@users.noreply.github.com>
2021-08-15 20:22:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -260,34 +260,6 @@ if mode == "欲求フラグ判定結果":
         download_button_str = download_button(sample_dtypes[s], filename, f'Click here to download {filename}', pickle_it=False)
         st.markdown(download_button_str, unsafe_allow_html=True)
     
-    '''
-    ***
-    '''
-    '''
-    if st.checkbox('データをダウンロードするにはチェックを入れてください'):
-        #df_table_view = df_table_view.drop('設問', axis=1) 
-        df_table_view = df_table_view.rename(columns={'personal score': '個人スコア'})
-        df_table_view = df_table_view.rename(columns={'average score': '平均スコア'})
-        df_table_view = df_table_view.rename(columns={'Difference': '差分'})
-        # Enter text for testing
-        s = 'pd.DataFrame'
-
-        filename = st.text_input('Enter output filename and ext (e.g. data.csv, )', 'data.csv')
-        #pickle_it = st.checkbox('Save as pickle file')
-        sample_dtypes = {'list': [1,'a', [2, 'c'], {'b': 2}],
-                         'str': 'Hello Streamlit!',
-                         'int': 17,
-                         'float': 17.0,
-                         'dict': {1: 'a', 'x': [2, 'c'], 2: {'b': 2}},
-                         'bool': True,
-                         'pd.DataFrame': df_table_view.T}
-        sample_dtypes = sample_dtypes
-
-        # Download sample
-        download_button_str = download_button(sample_dtypes[s].T, f'Click here to download {filename}', pickle_it=False)
-        st.markdown(download_button_str, unsafe_allow_html=True)
-     '''
-
 #============================================================================
 #                       1. 欲求判定設問（全23問）
 #============================================================================
